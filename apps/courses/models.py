25f864cc0501ac99a7dd7d529f5fa74037eece2e
Marcus Sakoda <Jubhub@yahool.com>
2017-01-26 12:36:52 -0800

    initial commit

---
+++ apps/courses/models.py
@@ -0,0 +1,33 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class CourseManager(models.Manager):
+    def validate_inputs(request):
+        errors = []
+        course_list = Course.objects.filter(course_name=request.POST['course_name'])
+        if len(course_list)>0:
+            errors.append("That course already exists")
+        else:
+            if len(request.POST['description'])<1:
+                errors.append("Please enter a description")
+        if errors:
+            return (False, errors)
+        else:
+            return (True, request)
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=255)
+    date_added = models.DateTimeField(auto_now_add = True)
+    objects = CourseManager()
+
+class Description(models.Model):
+    description = models.TextField()
+    course = models.ForeignKey(Course)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=255)
+    course = models.ForeignKey(Course)
+    created_at = models.DateTimeField(auto_now_add = True)
